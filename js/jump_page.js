d90284d1b83e312aec4f8cec4287133460036ff4
Juan Maswell Chinchilla Abarca <jchinchillaa@ucenfotec.ac.cr>
2020-05-04 18:01:39 -0600

    some things added

---
+++ js/jump_page.js
@@ -0,0 +1,16 @@
+
+$('#skills_anchor').click(function () {
+    $('html,body').animate({
+        scrollTop: $(".skills").offset().top},
+        'slow');
+})
+$('#knowledge_anchor').click(function () {
+    $('html,body').animate({
+        scrollTop: $(".knowledge").offset().top},
+        'slow');
+})
+$('#about_me_anchor').click(function () {
+    $('html,body').animate({
+        scrollTop: $(".about_me").offset().top},
+        'slow');
+})
